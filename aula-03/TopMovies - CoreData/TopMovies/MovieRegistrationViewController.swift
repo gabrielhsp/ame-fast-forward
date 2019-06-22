ff02a3dda810643f01d428f82353f045f5914917
Gabriel Henrique <gabhenrique0@gmail.com>
2019-06-22 13:00:33 -0300

    Create the MovieRegistration view controller and storyboard screen

---
+++ aula-03/TopMovies - CoreData/TopMovies/MovieRegistrationViewController.swift
@@ -0,0 +1,15 @@
+//
+//  MovieRegistrationViewController.swift
+//  TopMovies
+//
+//  Created by Gabriel Pereira on 22/06/19.
+//  Copyright © 2019 Gabriel Pereira. All rights reserved.
+//
+
+import UIKit
+
+class MovieRegistrationViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
